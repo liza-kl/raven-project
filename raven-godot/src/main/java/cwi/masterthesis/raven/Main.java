960f8f19ae29f469a8e0b638d0f57ef5438dfc06
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2024-05-11 11:08:03 +0200

    chore(rascal-protocol) taking json string for traversal

--- raven-godot/src/main/java/cwi/masterthesis/raven/Main.java
+++ raven-godot/src/main/java/cwi/masterthesis/raven/Main.java
@@ -6,50 +6,68 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import cwi.masterthesis.raven.application.client.ClientFactory;
 import cwi.masterthesis.raven.application.client.NativeClient;
 import cwi.masterthesis.raven.application.client.StandardClientFactory;
-import cwi.masterthesis.raven.files.FileUtils;
 import cwi.masterthesis.raven.interpreter.mapper.RavenJSONTraverser;
-import godot.FileAccess;
 import godot.Node;
 import godot.annotation.RegisterClass;
 import godot.annotation.RegisterFunction;
-import godot.core.NodePath;
+import godot.annotation.RegisterSignal;
+import godot.core.Callable;
+import godot.core.StringNameUtils;
+import godot.global.GD;
+import godot.signals.Signal;
+import godot.signals.SignalProvider;
 
 @RegisterClass
 public class Main extends Node {
-    public Node mainNode = getNode(new NodePath("."));
-    ClientFactory clientFactory = new StandardClientFactory();
+    public Node mainNode;
+
+    @RegisterSignal
+    public Signal mainUpdateScene = SignalProvider.signal(this, "main_update_scene");
+
+    @RegisterFunction
+    public void callbackUpdateScene(String jsonSpec) {
+        traverseJSON(jsonSpec, this.mainNode.getChild(0));
+    }
+
+        ClientFactory clientFactory = new StandardClientFactory();
   //  Client client = clientFactory.createClientFromType("native");
-    NativeClient client = NativeClient.getInstance("0.0.0.0", 23000);
+    NativeClient client;
     @RegisterFunction
     @Override
     public void _process(double delta) {
-       client.virtualProcess();
+   //    client.virtualProcess();
     }
 
+    // The goal is that everytime we get a new json into the buffer to parse it.
     @RegisterFunction
     @Override
     public void _ready() {
+        this.mainNode = getTree().getRoot();
+        this.client = NativeClient.getInstance("0.0.0.0", 23000, this.mainNode);
+        GD.INSTANCE.print("main Node in Main Class" + this.mainNode);
         client.virtualReady();
         String sceneTreePath = "/Users/ekletsko/raven-project/raven-core/src/main/rascal/tree.json";
-        FileUtils.createAProtocolFile();
-        getRavenJSONTraverser(sceneTreePath);
+     //   traverseJSON(sceneTreePath,  this.mainNode.getChild(0));
+
+        connect(
+                StringNameUtils.asStringName("main_update_scene"),
+                new Callable(this, StringNameUtils.asStringName("callback_update_scene"))
+        );
     }
 
-    private void getRavenJSONTraverser(String sceneTreePath) {
-        String exampleRequest = FileAccess.Companion.getFileAsString(sceneTreePath);
-
-        RavenJSONTraverser traverser = new RavenJSONTraverser();
-
+    public static void traverseJSON(String sceneTreeJSON, Node mainNode) {
+      //String exampleRequest = FileAccess.Companion.getFileAsString(sceneTreeJSON);
+        GD.INSTANCE.print("Traverse JSON is called");
         ObjectMapper mapper = new ObjectMapper();
         JsonNode rootNode;
         try {
-            rootNode = mapper.readTree(exampleRequest);
+            rootNode = mapper.readTree(sceneTreeJSON);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
 
         try {
-            traverser.traverse(rootNode, this.mainNode);
+            RavenJSONTraverser.traverse(rootNode, mainNode);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
