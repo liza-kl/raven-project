53bd1f29d40a3046fb7a89dbc8930188dbd372b3
Elisabeth Kletsko <elisabeth.kletsko@inovex.de>
2024-06-17 20:05:50 +0200

    feat(raven-godot): Add MarginContainer

---
+++ raven-godot/src/main/java/cwi/masterthesis/raven/interpreter/nodes/control/RavenMarginContainer.java
@@ -0,0 +1,43 @@
+package cwi.masterthesis.raven.interpreter.nodes.control;
+
+import cwi.masterthesis.raven.interpreter.Visitor;
+import cwi.masterthesis.raven.interpreter.nodes.RavenNode;
+import godot.Node;
+
+public class RavenMarginContainer implements RavenNode {
+
+    private final String nodeID;
+    private final Node parentNode;
+    private final String name;
+    private final String styles;
+
+    public RavenMarginContainer(String nodeID, Node parentNode, String name, String styles) {
+        this.nodeID = nodeID;
+        this.parentNode = parentNode;
+        this.name = name;
+        this.styles = styles;
+    }
+
+    @Override
+    public String getNodeID() {
+        return this.nodeID;
+    }
+
+    @Override
+    public Node getParentNode() {
+        return this.parentNode;
+    }
+
+    @Override
+    public void acceptVisitor(Visitor visitor) {
+        visitor.visitMarginContainer(this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStyles() {
+        return styles;
+    }
+}
